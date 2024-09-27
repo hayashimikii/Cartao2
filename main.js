5d2bfbee513efe40cbfd7f02a6213fad198a0066
hayashimikii <142352561+hayashimikii@users.noreply.github.com>
2024-09-27 11:57:13 -0300

    p

--- main.js
+++ main.js
@@ -8,6 +8,6 @@ function areaQuadrado(lado) {
 }
 console.log (areaQuadrado(6));
 let media = function (num1, num2, num3, num4) {
-    return num1+num2+num3/num4
+    return num1+num2+num3/num4;
 }
 console.log (soma (1,2,3/3));
