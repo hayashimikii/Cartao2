2dc827b89da6d88c28726608e2192119ca4187d3
hayashimikii <142352561+hayashimikii@users.noreply.github.com>
2024-09-27 12:18:31 -0300

    0

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ function areaQuadrado(lado) {
     return lado**2;
 }
 console.log (areaQuadrado(6));
-let media= function (num1, num2, num3, num4) {
-    return (num1+num2+num3)/num4;
+let media= function (num1, num2, num3) {
+    return (num1+num2+num3)/3;
 }
 console.log (media(1,2,3));
 
