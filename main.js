29dce955e3936b7c6299b304495d2edda03c8ef7
hayashimikii <142352561+hayashimikii@users.noreply.github.com>
2024-09-27 12:14:16 -0300

    0

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ console.log (areaQuadrado(6));
 let media= function (num1, num2, num3, num4) {
     return (num1+num2+num3)/num4;
 }
-console.log (media(1,2,3,3));
+console.log (media(1,2,3,));
 
 let soma= prompt('qual o nome?');
 document.write(nome);
