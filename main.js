953a6bd88685348124d7665a73372f6b8924d5ef
hayashimikii <142352561+hayashimikii@users.noreply.github.com>
2024-10-04 11:26:36 -0300

    d

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
- function soma(num1, num2) {
-    return num1+num2;
-}
- console.log(soma(6,4));
+//  function soma(num1, num2) {
+//     return num1+num2;
+// }
+//  console.log(soma(6,4));
 
- function areaQuadrado(lado) {
-     return lado**2;
- }
- console.log (areaQuadrado(6));
- let media= function (num1, num2, num3) {
-     return (num1+num2+num3)/3;
- }
- console.log (media(1,2,3));
- let num1 = parseInt (prompt('Qual n?'));
- let num2 = parseInt (prompt('Qual n?'));
- let num3 = parseInt (prompt('Qual n?'));
- let media1 = function (x,y,z){
-     return (x+y+z)/3;
- };
- document.write(media1(num1, num2, num3));
+//  function areaQuadrado(lado) {
+//      return lado**2;
+//  }
+//  console.log (areaQuadrado(6));
+//  let media= function (num1, num2, num3) {
+//      return (num1+num2+num3)/3;
+//  }
+//  console.log (media(1,2,3));
+//  let num1 = parseInt (prompt('Qual n?'));
+//  let num2 = parseInt (prompt('Qual n?'));
+//  let num3 = parseInt (prompt('Qual n?'));
+//  let media1 = function (x,y,z){
+//      return (x+y+z)/3;
+//  };
+//  document.write(media1(num1, num2, num3));
+
+
+let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'
+
+
+
+]
