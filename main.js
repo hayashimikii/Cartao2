ba770ceae2e4dee5decfbc8be39c0a909dc2d85c
hayashimikii <142352561+hayashimikii@users.noreply.github.com>
2024-10-04 11:17:51 -0300

    o

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-function soma(num1, num2) {
+ function soma(num1, num2) {
     return num1+num2;
 }
-console.log(soma(6,4));
+ console.log(soma(6,4));
 
-function areaQuadrado(lado) {
-    return lado**2;
-}
-console.log (areaQuadrado(6));
-let media= function (num1, num2, num3) {
-    return (num1+num2+num3)/3;
-}
-console.log (media(1,2,3));
-let num1 = parsent (prompt('Qual n?'));
-let num2 = parsent (prompt('Qual n?'));
-let num3 = parsent (prompt('Qual n?'));
-function media (x,y,z){
-    return (x=y=z)/3,
-}
-document.write(media(num1, num2, num3));
+ function areaQuadrado(lado) {
+     return lado**2;
+ }
+ console.log (areaQuadrado(6));
+ let media= function (num1, num2, num3) {
+     return (num1+num2+num3)/3;
+ }
+ console.log (media(1,2,3));
+ let num1 = parsent (prompt('Qual n?'));
+ let num2 = parsent (prompt('Qual n?'));
+ let num3 = parsent (prompt('Qual n?'));
+ function media (x,y,z){
+     return (x+y+z)/3,
+ };
+ document.write(media(num1, num2, num3));
