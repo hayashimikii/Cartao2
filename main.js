efb33449d3ec402c4b7c926c26ae821b9960cc3c
hayashimikii <142352561+hayashimikii@users.noreply.github.com>
2024-10-04 06:52:46 -0300

    v

--- main.js
+++ main.js
@@ -11,4 +11,14 @@ let media= function (num1, num2, num3) {
     return (num1+num2+num3)/3;
 }
 console.log (media(1,2,3));
-
+<head>
+    <title>Media de Numeros</title>
+    <script>
+        function media(a,b,c) {
+            return (a+b+c)/3;
+         }
+         let materia = promp('Qual a materia') ;
+         document.write('Matematica:'+ materia);
+         console.log('A media é:'=media(1,2,3));
+    </script>
+</head>
